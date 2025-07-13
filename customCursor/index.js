875a45f471bb4f77a312200ec40d251c4ab7a247
waraton <bensonwaran@gmail.com>
2025-07-14 00:48:02 +0300

    Added custom cursor

---
+++ customCursor/index.js
@@ -0,0 +1,14 @@
+const cursor = document.body.querySelector('.cursor')
+const output = document.body.querySelector('.output')
+
+document.body.addEventListener('mousemove',e =>{
+  cursor.style.left = `${e.x -5}px`
+  cursor.style.top = `${e.y -5}px`
+  
+  document.querySelector('.page').textContent = `${e.pageX} | ${e.pageY}`
+  document.querySelector('.screen').textContent = `${e.screenX} | ${e.screenY}`
+  document.querySelector('.offset').textContent = `${e.offsetX} | ${e.offsetY}`
+  document.querySelector('.xy').textContent = `${e.x} | ${e.y}`
+  document.querySelector('.movement').textContent = `${e.movementX} | ${e.movementY}`
+  document.querySelector('.client').textContent = `${e.clientX} | ${e.clientY}`
+})
