8ab76cfc51970944a91bffcc9e51046dcb9c89e4
waraton <bensonwaran@gmail.com>
2025-07-27 16:09:50 +0300

    +labels and section show hide password

--- Random_password/random_password.js
+++ Random_password/random_password.js
@@ -2,11 +2,15 @@ const generatePassword = document.querySelector(".generate");
 const copyPassword = document.querySelector(".copy");
 const lengthPassword = document.querySelector("#pLength");
 const chars = `1234567890-=qwertyuio[\]asdfghjkl;'zxcvbnm,./><?:"{}_+!@~#$%^&*()QWERTYUIOPASDFGHJKLZXCVBNM`;
-let output = document.querySelector('.output')
+let output = document.querySelector(".output");
 let password = "";
+
+const showOrHide = document.getElementById("showOrHide");
+
 generatePassword.disabled = true;
 copyPassword.disabled = true;
-output.textContent = 'Select length..'
+output.textContent = "Select length..";
+output.classList.add('error')
 
 generatePassword.addEventListener("click", generate);
 
@@ -15,10 +19,12 @@ lengthPassword.addEventListener("input", () => {
     output.textContent = "Enter a length > 8";
     generatePassword.disabled = true;
     copyPassword.disabled = true;
+    output.classList.add('error')
   } else {
     copyPassword.disabled = false;
     generatePassword.disabled = false;
-    output.textContent = 'Click generate'
+    output.textContent = "Click generate";
+    output.classList.remove('error')
   }
 });
 
@@ -38,3 +44,15 @@ function generate() {
 function copy() {
   navigator.clipboard.writeText(document.querySelector("output").textContent);
 }
+
+function showHide() {
+  showOrHide.type = showOrHide.type === "text" ? "password" : "text";
+  document.querySelector("[show]").textContent =
+    showOrHide.type === "text" ? "Hide password" : "Show password";
+}
+
+function paste() {
+  navigator.clipboard.readText().then((text) => (showOrHide.value = text));
+}
+document.querySelector("[paste]").addEventListener("click", paste);
+document.querySelector("[show]").addEventListener("click", showHide);
