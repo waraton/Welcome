689f77f259f3df4c0f11d65eed7c00ae8585f5a7
waraton <bensonwaran@gmail.com>
2025-08-15 15:12:19 +0300

    image viewer added

---
+++ image_viewer/app.js
@@ -0,0 +1,18 @@
+const load = document.querySelector("input");
+const img = document.querySelector("img");
+const close = document.querySelector(".icon .close");
+
+load.addEventListener("change", (e) => {
+  loadFile(e);
+});
+
+function loadFile(event) {
+  img.classList.add("active");
+  close.classList.add("exit");
+  img.src = URL.createObjectURL(event.target.files[0]);
+
+  close.addEventListener("click", () => {
+    img.classList.toggle("active");
+    close.classList.toggle("exit");
+  });
+}
