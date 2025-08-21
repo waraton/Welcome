2bcce3650f41774c6aa6e351c5fb8559f51ea5d9
waraton <bensonwaran@gmail.com>
2025-08-21 09:49:28 +0300

    added animations

---
+++ animations/animating_text/app.js
@@ -0,0 +1,12 @@
+const animationText = document.getElementById('animation');
+const text = animationText.textContent.split('');
+animationText.textContent = ''
+text.forEach(letter => {
+  animationText.innerHTML += `<span class="letter">${letter}</span>`
+})
+
+document.querySelectorAll('.letter').forEach((char, index) => {
+  char.style.animationDelay = `${index/4}s`
+  char.style.animationDuration = `${Math.random()*2+5}s`
+  char.style.color = `hsl(${Math.random()*360},50%,50%)`
+})
