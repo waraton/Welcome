db225104f9a1dfb6278e3b35ae88a131b0fd2b6e
waraton <bensonwaran@gmail.com>
2025-10-11 22:28:18 +0300

    new feature

--- Text Editing/text_edit.js
+++ Text Editing/text_edit.js
@@ -1,34 +1,34 @@
-const text = document.getElementById('text');
+const text = document.getElementById("text");
+const uppercase = document.getElementById("uppercase");
+const lowercase = document.getElementById("lowercase");
+const bold = document.getElementById("bold");
+const italic = document.getElementById("italic");
+const underline = document.getElementById("underline");
+const capitalize = document.getElementById("capitalize");
+const output = document.querySelector("output");
 
-const uppercase = document.getElementById('uppercase');
-const lowercase = document.getElementById('lowercase');
-const bold = document.getElementById('bold');
-const italic = document.getElementById('italic');
-const underline = document.getElementById('underline');
-const capitalize = document.getElementById('capitalize');
-const output = document.querySelector('output')
+const button = document.querySelectorAll("button");
+editText(button);
 
-const button = document.querySelectorAll('button')
-editText(button)
-
-function editText(button){
-    button.forEach((btn,ind)=>{
-        btn.addEventListener('click',()=>{
-            output.classList.remove('uppercase','lowercase','capitalize')
-            if (ind < 3){
-                button[0].classList.remove('active')
-                button[1].classList.remove('active')
-                button[2].classList.remove('active')
-                output.classList.toggle(btn.id)
-                btn.classList.toggle('active')
-        } else{ 
-            btn.classList.toggle('active')
-            output.classList.toggle(btn.id)
-        }
-    })
-    })
+function editText(button) {
+  button.forEach((btn, ind) => {
+    btn.addEventListener("click", () => {
+      output.textContent = output.textContent.toLocaleLowerCase();
+      if (ind < 3) {
+        output.classList.remove("uppercase", "lowercase", "capitalize");
+        button[0].classList.remove("active");
+        button[1].classList.remove("active");
+        button[2].classList.remove("active");
+        output.classList.toggle(btn.id);
+        btn.classList.toggle("active");
+      } else {
+        btn.classList.toggle("active");
+        output.classList.toggle(btn.id);
+      }
+    });
+  });
 }
 
-text.addEventListener('input',()=>{
-    output.textContent = text.value
-})
+text.addEventListener("input", () => {
+  output.textContent = text.value;
+});
