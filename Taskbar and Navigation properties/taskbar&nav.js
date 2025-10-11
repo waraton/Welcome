89b2557a29a835ff2e532a4ff6be215cae524f1f
waraton <bensonwaran@gmail.com>
2025-10-11 22:28:05 +0300

    enabled inactive mode when click outside

--- Taskbar and Navigation properties/taskbar&nav.js
+++ Taskbar and Navigation properties/taskbar&nav.js
@@ -1,29 +1,38 @@
-const navButtons = document.querySelectorAll('nav button');
-const forms = document.querySelectorAll('form');
+const ETarget = document.querySelector("body > section");
+const auto = document.querySelector("[auto]");
+const navButtons = document.querySelectorAll("nav button");
+const forms = document.querySelectorAll("form");
+const autoDependant = document.querySelector("[autoDependant]");
 
-navButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        navButtons.forEach(btn => btn.classList.remove('running'))
+navButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    navButtons.forEach((btn) => btn.classList.remove("running"));
+    
+    button.classList.add("running");
+    console.log(button.dataset);
+    
+    forms.forEach((form) => {
+      if (form.id === button.dataset.mapped) form.classList.add("active");
+      else form.classList.remove("active");
+    });
+  });
+});
 
-        button.classList.add('running')
-        console.log(button.dataset);
+auto.addEventListener("change", () => {
+  if (!auto.checked) {
+    autoDependant.classList.add("disabled");
+    autoDependant.firstElementChild.disabled = true;
+  } else {
+    autoDependant.classList.remove("disabled");
+    autoDependant.firstElementChild.checked = false;
+    autoDependant.firstElementChild.disabled = false;
+  }
+});
 
-        forms.forEach((form, index) => {
-            if (form.id === button.dataset.mapped) form.classList.add('active');
-            else form.classList.remove('active');
-        })
-    })
-})
-
-const auto = document.querySelector('[auto]')
-const autoDependant = document.querySelector('[autoDependant]')
-auto.addEventListener('change', () => {
-    if (!auto.checked) {
-        autoDependant.classList.add('disabled');
-        autoDependant.firstElementChild.disabled = true
-    } else {
-        autoDependant.classList.remove('disabled');
-        autoDependant.firstElementChild.checked = false
-        autoDependant.firstElementChild.disabled = false
-    }
-})
+document.addEventListener("click", (E) => {
+  if (ETarget.contains(E.target)) {
+    ETarget.classList.remove("deactivated");
+  } else {
+    ETarget.classList.add("deactivated");
+  }
+});
