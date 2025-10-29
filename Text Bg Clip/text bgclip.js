5a36f6dbec467b5c18e5b03c526a747b1910f531
waraton <bensonwaran@gmail.com>
2025-10-29 20:05:04 +0300

    position dependent zoom

--- Text Bg Clip/text bgclip.js
+++ Text Bg Clip/text bgclip.js
@@ -9,3 +9,15 @@ button.addEventListener('click',()=>{
 const bgColor = ()=>{
     return `RGB(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`
 }
+
+document.querySelector(`figure img`).addEventListener(`mouseenter`,(e)=>{
+    e.target.classList.add(`hovered`)
+})
+document.querySelector(`figure img`).addEventListener(`mouseleave`,(e)=>{
+    e.target.classList.remove(`hovered`)
+})
+document.querySelector(`figure img`).addEventListener(`mousemove`,(e)=>{
+    const [x,y] = [e.offsetX,e.offsetY]
+/*     console.log(`< x:`+x+`<>`+y+`: y>`); */
+    e.target.style.transformOrigin = `${x}px ${y}px`
+})
