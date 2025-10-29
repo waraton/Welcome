56395ba149e6564e887c41ec7894c87711d71230
waraton <bensonwaran@gmail.com>
2025-10-29 20:00:37 +0300

    searching tool

--- Read more/app.js
+++ Read more/app.js
@@ -1,3 +1,48 @@
-document.querySelector('article button').addEventListener('click',()=>{
-    document.querySelectorAll('.more').forEach(more => more.classList.toggle('toggled'))
-})
+document
+  .querySelector("article button")
+  .addEventListener("click", (e) =>
+    document
+      .querySelectorAll(".more")
+      .forEach((more) => {
+        more.classList.toggle("toggled")
+        e.target.textContent = more.classList.contains('toggled') ? 'Collapse':'Expand'
+    })
+  );
+
+function search() {
+  let searchTerm = document.querySelector("div:has([type=search]) input").value;
+
+  document
+    .querySelectorAll(".more")
+    .forEach((more) => more.classList.add("toggled"));
+
+  if (searchTerm !== "") {
+    let regex = new RegExp(searchTerm, "gi");
+    document
+      .querySelectorAll("article[text] *")
+      .forEach(
+        (item) =>
+          (item.innerHTML = item.textContent.replace(regex, "<mark>$&</mark>"))
+      );
+
+    let item = document.querySelector("[result]");
+    item.textContent = "";
+    const searchTerms = document.querySelectorAll("mark");
+    searchTerms.forEach((a, b) => {
+      const text = a.innerText;
+      a.id = b;
+      item.innerHTML += `<p>${b + 1}. <a href=#${b}>${text}</a></p>`;
+    });
+  }
+}
+
+document
+  .querySelector("div:has([type=search]) button:first-of-type")
+  .addEventListener("click",()=>{search()});
+
+document
+  .querySelector("div:has([type=search]) *:first-child")
+  .addEventListener("keypress",(e)=>{
+    if (e.key == 'Enter') search()
+    }
+);
