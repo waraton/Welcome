e70ae0b62b1128166947097618b5195271db8f38
waraton <bensonwaran@gmail.com>
2026-01-04 20:56:08 +0300

    can get vers and or verses

--- BibleSearch/app.js
+++ BibleSearch/app.js
@@ -1,34 +1,35 @@
+const verse = document.querySelector(`.verse`);
+const reference = document.querySelector(`.reference`);
 const inputs = document.querySelectorAll(`form input`);
 const button = document.querySelector(`form button`);
+const datalist = document.querySelector(`datalist`);
 
 button.addEventListener(`click`, (e) => {
-  e.preventDefault()
+  e.preventDefault();
   if (
     inputs[0].value !== `` &&
     inputs[1].value !== `` &&
     inputs[2].value !== ``
   ) {
-    fetchJson(
-      inputs[0].value,
-      inputs[1].value,
-      inputs[2].value
-    );
+    fetchJson(inputs[0].value, inputs[1].value, inputs[2].value);
   }
 });
 
 async function fetchJson(BOOK, CHAPTER, VERSE) {
-  await fetch(`https://bible-api.com/${BOOK}\s${CHAPTER}:${VERSE}`)
-    .then((ar) => {
-      console.log(ar);
-      ar.json();
-    })
-    .then((data1) => {
-      console.log(data1);
-    });
+  try {
+    const res = await fetch(
+      `https://bible-api.com/${BOOK} ${CHAPTER}:${VERSE}`
+    );
+    const data = await res.json();
 
-  await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${BOOK}`)
-    .then((res) => {
-      console.log(res);
-      res.json()
-    })
+    verse.textContent = data.text;
+    reference.textContent = `${
+      data.reference
+    }(${data.translation_id.toUpperCase()})`;
+  } catch (error) {
+    console.error(error);
+  }
+  /* const res2 = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${BOOK}`)
+  const  data2 = await res2.json()
+   console.table(data2); */
 }
