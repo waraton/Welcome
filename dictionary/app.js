7d482db5f37d4b0ad83dd7fdec060bd86d1e36e3
waraton <bensonwaran@gmail.com>
2026-01-05 15:28:01 +0300

    attach app.js to index.html as a module

--- dictionary/app.js
+++ dictionary/app.js
@@ -1,10 +1,12 @@
+import { testData2, renderDefinitions } from "./testData.js";
 /**
  *
  * @param {string} WORD Word whose meaning is to be fetched
- * @const {JSON} response response to user request
- * @const {Object} data data returned to the user that can be displayed to the front end
+ * @constant {object} response response to user request
+ * @const {Array} data data returned to the user that can be displayed to the front end
+ * @function  fetchDictionaryDefinition used to send data to api and retrieve responce
  */
-
+renderDefinitions(testData2);
 const WORD = document.querySelector(`input[id='word']`);
 
 document
@@ -13,16 +15,31 @@ document
     e.preventDefault();
     if (WORD.value) {
       fetchDictionaryDefinition(WORD.value);
-    }
+    } else document.querySelector(`article#dict`).textContent += `ERROR1`;
   });
 
 async function fetchDictionaryDefinition(WORD) {
-  const response = await fetch(
-    `https://api.dictionaryapi.dev/api/v2/entries/en/${WORD}`
-  );
-  const data = await response.json();
-  console.log(data);
-  document.querySelector(
-    `article`
-  ).innerHTML = JSON.stringify(data);
+  try {
+    const response = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${WORD}`
+    );
+    const data = await response.json();
+
+    if (response.ok) {
+      console.log(response);
+      renderDefinitions(data);
+    } else {
+      console.log(response);
+      document.querySelector(
+        `article#dict`
+      ).innerHTML = `<h2>${response.status}: ${data.title}</h2>
+      <p>${data.message}</p>
+      <p>${data.resolution}</p>`;
+    }
+  } catch (error) {
+    document.querySelector(
+      `article`
+    ).textContent = `ERROR ocurred. Check console`;
+    throw new Error(`ERROR >>`, error);
+  }
 }
