9008a25e9e04b38838976870ade1317d493ccf75
waraton <bensonwaran@gmail.com>
2026-01-05 08:21:11 +0300

    added datalist

--- BibleSearch/app.js
+++ BibleSearch/app.js
@@ -33,3 +33,84 @@ async function fetchJson(BOOK, CHAPTER, VERSE) {
   const  data2 = await res2.json()
    console.table(data2); */
 }
+
+const bibleBooks = [
+  ["Genesis", 50, 1533],
+  ["Exodus", 40, 1213],
+  ["Leviticus", 27, 859],
+  ["Numbers", 36, 1288],
+  ["Deuteronomy", 34, 959],
+  ["Joshua", 24, 658],
+  ["Judges", 21, 618],
+  ["Ruth", 4, 85],
+  ["1 Samuel", 31, 810],
+  ["2 Samuel", 24, 695],
+  ["1 Kings", 22, 816],
+  ["2 Kings", 25, 719],
+  ["1 Chronicles", 29, 942],
+  ["2 Chronicles", 36, 822],
+  ["Ezra", 10, 280],
+  ["Nehemiah", 13, 406],
+  ["Esther", 10, 167],
+  ["Job", 42, 1070],
+  ["Psalms", 150, 2461],
+  ["Proverbs", 31, 915],
+  ["Ecclesiastes", 12, 222],
+  ["Song of Solomon", 8, 117],
+  ["Isaiah", 66, 1292],
+  ["Jeremiah", 52, 1364],
+  ["Lamentations", 5, 154],
+  ["Ezekiel", 48, 1273],
+  ["Daniel", 12, 357],
+  ["Hosea", 14, 197],
+  ["Joel", 3, 73],
+  ["Amos", 9, 146],
+  ["Obadiah", 1, 21],
+  ["Jonah", 4, 48],
+  ["Micah", 7, 105],
+  ["Nahum", 3, 47],
+  ["Habakkuk", 3, 56],
+  ["Zephaniah", 3, 53],
+  ["Haggai", 2, 38],
+  ["Zechariah", 14, 211],
+  ["Malachi", 4, 55],
+  ["Matthew", 28, 1071],
+  ["Mark", 16, 678],
+  ["Luke", 24, 1151],
+  ["John", 21, 879],
+  ["Acts", 28, 1007],
+  ["Romans", 16, 433],
+  ["1 Corinthians", 16, 437],
+  ["2 Corinthians", 13, 257],
+  ["Galatians", 6, 149],
+  ["Ephesians", 6, 155],
+  ["Philippians", 4, 104],
+  ["Colossians", 4, 95],
+  ["1 Thessalonians", 5, 89],
+  ["2 Thessalonians", 3, 47],
+  ["1 Timothy", 6, 113],
+  ["2 Timothy", 4, 83],
+  ["Titus", 3, 46],
+  ["Philemon", 1, 25],
+  ["Hebrews", 13, 303],
+  ["James", 5, 108],
+  ["1 Peter", 5, 105],
+  ["2 Peter", 3, 61],
+  ["1 John", 5, 105],
+  ["2 John", 1, 13],
+  ["3 John", 1, 14],
+  ["Jude", 1, 25],
+  ["Revelation", 22, 404],
+];
+
+datalist.id = `books`;
+bibleBooks.forEach((book) => {
+  datalist.innerHTML += `<option value='${book[0].toLowerCase()}' data-max=${book[1]}></option>`;
+});
+
+inputs[1].addEventListener(`input`, () => {
+  const in1 = inputs[0].value.toLowerCase();
+  const max = document.querySelector(`datalist *[value='${in1}']`);
+  inputs[1].max = max.dataset.max;
+  inputs[1].title = `Maximum chapters: ${max.dataset.max}`;
+});
