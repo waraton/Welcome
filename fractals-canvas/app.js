4665718e9128239d7726b5a3459991ec11d90fb7
waraton <bensonwaran@gmail.com>
2026-01-06 19:10:40 +0300

    chaotic scaling

--- fractals-canvas/app.js
+++ fractals-canvas/app.js
@@ -6,7 +6,7 @@ const SIZE = 600;
 canvas.width = SIZE;
 canvas.height = SIZE;
 ctx.lineCap = 'round'
-ctx.shadowColor = '#2fa2'
+ctx.shadowColor = '#222a'
 ctx.shadowBlur = 8
 ctx.shadowOffsetX = 5
 ctx.shadowOffsetY = 5
@@ -24,12 +24,12 @@ ctx.shadowOffsetY = 5
 
 function drawFractal() {
   const hue = Math.random() * 360;
-  const lineWidth = Math.random() * 11 + 5;
-  const SIDES = 7;
-  const maxLevel = 4;
-  const diverge = Math.random() * 1 - 0.5;
-  const scale = Math.random() * 0.2 + 0.4;
-  const branches = 4;
+  const lineWidth = Math.random() * 5 + 2;
+  const SIDES = 5;
+  const maxLevel = 5;
+  const diverge = Math.random() * 2 - 1;
+  const scale = Math.random() * 0.1 + 0.4;
+  const branches = 4 ;
 
   ctx.clearRect(0, 0, SIZE, SIZE);
   ctx.lineWidth = lineWidth;
@@ -45,7 +45,7 @@ function drawFractal() {
 function drawBranch(level, maxLevel, diverge, scale, branches, hue) {
   if (level > maxLevel) return;
   const branchSize = SIZE * 0.25;
-  let lightness = 8 + level * 8
+  let lightness = 5 + level * 15
   ctx.strokeStyle = `hsl(${hue}, 75%, ${lightness}%)`;
   ctx.beginPath();
   ctx.moveTo(0, 0);
@@ -53,11 +53,11 @@ function drawBranch(level, maxLevel, diverge, scale, branches, hue) {
   ctx.stroke();
 
   for (let i = 0; i < branches; i++) {
-    const ref = branchSize - (branchSize / branches) * i;
+    const ref = branchSize - (branchSize / branches) *  i;
     ctx.save();
     ctx.translate(ref, 0);
-    ctx.scale(scale, scale, hue);
-    ctx.rotate(diverge);
+    ctx.scale(scale, scale);
+    ctx.rotate(diverge * (i - (branches - diverge * 5 + Math.random() * 1.5) / 2));
     drawBranch(level + 1, maxLevel, diverge, scale, branches, hue);
     ctx.restore();
   }
