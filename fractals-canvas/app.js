00bc30c93d8de7840efb42f3a0d9737045974615
waraton <bensonwaran@gmail.com>
2026-01-06 20:24:24 +0300

    classed fractal

--- fractals-canvas/app.js
+++ fractals-canvas/app.js
@@ -5,11 +5,11 @@ const mutate = document.getElementById("mutate");
 const SIZE = 600;
 canvas.width = SIZE;
 canvas.height = SIZE;
-ctx.lineCap = 'round'
-ctx.shadowColor = '#222a'
-ctx.shadowBlur = 8
-ctx.shadowOffsetX = 5
-ctx.shadowOffsetY = 5
+ctx.lineCap = "round";
+ctx.shadowColor = "#222a";
+ctx.shadowBlur = 8;
+ctx.shadowOffsetX = 5;
+ctx.shadowOffsetY = 5;
 /** PROPERTIES
  *
  * @constant SIDES define sides of the fractal
@@ -22,46 +22,57 @@ ctx.shadowOffsetY = 5
  * @constant hue define hue of the color of the lines of the branches
  */
 
-function drawFractal() {
-  const hue = Math.random() * 360;
-  const lineWidth = Math.random() * 5 + 5;
-  const SIDES = 6;
-  const maxLevel = 5;
-  const diverge = Math.random() * .5 + .5;
-  const scale = Math.random() * 0.1 + 0.7;
-  const branches = 4;
-
-  ctx.clearRect(0, 0, SIZE, SIZE);
-  ctx.lineWidth = lineWidth;
-  ctx.save();
-  ctx.translate(SIZE / 2, SIZE / 2);
-  for (let i = 0; i < SIDES; i++) {
-    drawBranch(0, maxLevel, diverge, scale, branches, hue);
-    ctx.rotate((Math.PI * 2) / SIDES);
+class Fractal {
+  // properties
+  constructor(drawMode) {
+    this.lineWidth = Math.random() * 5 + 5;
+    this.hue = Math.random() * 360;
+    this.SIDES = 6;
+    this.maxLevel = 5;
+    this.diverge = Math.random() * 0.5 + 0.5;
+    this.scale = Math.random() * 0.1 + 0.7;
+    this.branches = 4;
+    this.branchSize = SIZE * 0.125;
   }
-  ctx.restore();
-}
 
-function drawBranch(level, maxLevel, diverge, scale, branches, hue) {
-  if (level > maxLevel) return;
-  const branchSize = SIZE * 0.125;
-  let lightness = 15 + level * 15
-  ctx.strokeStyle = `hsl(${hue}, 50%, ${lightness}%)`;
-  ctx.beginPath();
-  ctx.moveTo(0, 0);
-  ctx.lineTo(branchSize, 0);
-  ctx.stroke();
-
-  for (let i = 0; i < branches; i++) {
-    const ref = branchSize - (branchSize / branches) *  i;
+  draw(ctx) {
+    ctx.clearRect(0, 0, SIZE, SIZE);
+    ctx.lineWidth = this.lineWidth;
     ctx.save();
-    ctx.translate(ref, 0);
-    ctx.scale(scale, scale);
-    ctx.rotate(diverge * i * 2 / branches - diverge);
-    drawBranch(level + 1, maxLevel, diverge, scale, branches, hue);
+    ctx.translate(SIZE / 2, SIZE / 2);
+    for (let i = 0; i < this.SIDES; i++) {
+      this.drawBranch(ctx, 0);
+      ctx.rotate((Math.PI * 2) / this.SIDES);
+    }
     ctx.restore();
   }
+
+  drawBranch(ctx, level) {
+    if (level > this.maxLevel) return;
+    let lightness = 15 + level * 15;
+    ctx.strokeStyle = `hsl(${this.hue}, 50%, ${lightness}%)`;
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(this.branchSize, 0);
+    ctx.stroke();
+
+    for (let i = 0; i < this.branches; i++) {
+      const ref = this.branchSize - (this.branchSize / this.branches) * i;
+      ctx.save();
+      ctx.translate(ref, 0);
+      ctx.scale(this.scale, this.scale);
+      ctx.rotate((this.diverge * i * 2) / this.branches - this.diverge);
+      this.drawBranch(ctx, level + 1);
+      ctx.restore();
+    }
+  }
 }
+
+function drawFractal() {
+  const fractal = new Fractal();
+  fractal.draw(ctx);
+}
+
 drawFractal();
 
 mutate.addEventListener("click", drawFractal);
