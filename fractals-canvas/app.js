d8d75836f725b9d362b5e26dfa3a59e494adb28e
waraton <bensonwaran@gmail.com>
2026-01-06 15:40:43 +0300

    fractal branching

--- fractals-canvas/app.js
+++ fractals-canvas/app.js
@@ -10,40 +10,36 @@ ctx.lineWidth = 8;
  *
  * @constant SIDES define sides of the fractal
  * @constant branchSize define length of lines of the fractal
+ * @constant maxLevel define maximum number os branches
  */
 const SIDES = 4;
+const maxLevel = 5
 
 function drawFractal() {
   ctx.save();
   ctx.translate(SIZE / 2, SIZE / 2);
   for (let i = 0; i < SIDES; i++) {
-    drawBranch(0);
+    drawBranch(0,maxLevel);
     ctx.rotate((Math.PI * 2) / SIDES);
   }
   ctx.restore();
 }
 
-function drawBranch(level) {
-  if (level > 6) return;
+function drawBranch(level,maxLevel) {
+  if (level > maxLevel) return;
   const branchSize = 100;
   ctx.beginPath();
   ctx.moveTo(0, 0);
   ctx.lineTo(branchSize, 0);
   ctx.stroke();
 
-  ctx.save()
-  ctx.translate(branchSize, 0);
-  ctx.scale(0.7, 0.7);
-
-  ctx.save();
-  ctx.rotate(0.8);
-  drawBranch(level + 1);
-  ctx.restore();
-
-  ctx.save();
-  ctx.rotate(-0.8);
-  drawBranch(level + 1);
-  ctx.restore();
-  ctx.restore();
+  for (let i = 0; i < 2; i++) {
+    ctx.save()
+    ctx.translate(branchSize, 0);
+    ctx.scale(0.7, 0.7);
+    ctx.rotate(1.2 * i);
+    drawBranch(level + 1,maxLevel);
+    ctx.restore();
+  }
 }
 drawFractal();
