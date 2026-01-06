80e42576e74fc338d7439e6e90187d1abf0471f0
waraton <bensonwaran@gmail.com>
2026-01-06 17:29:35 +0300

    fractal with random varables

--- fractals-canvas/app.js
+++ fractals-canvas/app.js
@@ -1,45 +1,57 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const mutate = document.getElementById("mutate");
 
-const SIZE = 600;
+const SIZE = 64;
 canvas.width = SIZE;
 canvas.height = SIZE;
 ctx.strokeStyle = "orange";
-ctx.lineWidth = 8;
+ctx.lineWidth = 1;
 /** PROPERTIES
  *
  * @constant SIDES define sides of the fractal
  * @constant branchSize define length of lines of the fractal
  * @constant maxLevel define maximum number os branches
+ * @constant diverge define how wide the branches are
+ * @constant scale define scaling factor of the branches
+ * @constant scale define number of the branches
  */
-const SIDES = 4;
-const maxLevel = 5
 
 function drawFractal() {
+  const SIDES = 3;
+  const maxLevel = 4;
+  const diverge = Math.random() * 1 - 0.5;
+  const scale = Math.random() * 0.4 + 0.4;
+  const branches = 6;
+
+  ctx.clearRect(0, 0, SIZE, SIZE);
   ctx.save();
   ctx.translate(SIZE / 2, SIZE / 2);
   for (let i = 0; i < SIDES; i++) {
-    drawBranch(0,maxLevel);
+    drawBranch(0, maxLevel, diverge, scale, branches);
     ctx.rotate((Math.PI * 2) / SIDES);
   }
   ctx.restore();
 }
 
-function drawBranch(level,maxLevel) {
+function drawBranch(level, maxLevel, diverge, scale, branches) {
   if (level > maxLevel) return;
-  const branchSize = 100;
+  const branchSize = SIZE * 0.25;
   ctx.beginPath();
   ctx.moveTo(0, 0);
   ctx.lineTo(branchSize, 0);
   ctx.stroke();
 
-  for (let i = 0; i < 2; i++) {
-    ctx.save()
-    ctx.translate(branchSize, 0);
-    ctx.scale(0.7, 0.7);
-    ctx.rotate(1.2 * i);
-    drawBranch(level + 1,maxLevel);
+  for (let i = 0; i < branches; i++) {
+    const ref = branchSize - (branchSize / branches) * i;
+    ctx.save();
+    ctx.translate(ref, 0);
+    ctx.scale(scale, scale);
+    ctx.rotate(diverge);
+    drawBranch(level + 1, maxLevel, diverge, scale, branches);
     ctx.restore();
   }
 }
 drawFractal();
+
+mutate.addEventListener("click", drawFractal);
