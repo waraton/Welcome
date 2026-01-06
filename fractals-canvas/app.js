6787c2d808e12b64f290f8a43032d3e382de5e05
waraton <bensonwaran@gmail.com>
2026-01-06 17:54:33 +0300

    hue the branches

--- fractals-canvas/app.js
+++ fractals-canvas/app.js
@@ -2,11 +2,9 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const mutate = document.getElementById("mutate");
 
-const SIZE = 64;
+const SIZE = 600;
 canvas.width = SIZE;
 canvas.height = SIZE;
-ctx.strokeStyle = "orange";
-ctx.lineWidth = 1;
 /** PROPERTIES
  *
  * @constant SIDES define sides of the fractal
@@ -14,29 +12,35 @@ ctx.lineWidth = 1;
  * @constant maxLevel define maximum number os branches
  * @constant diverge define how wide the branches are
  * @constant scale define scaling factor of the branches
- * @constant scale define number of the branches
+ * @constant branches define number of the branches
+ * @constant lineWidth define width of the lines of the branches
+ * @constant hue define hue of the color of the lines of the branches
  */
 
 function drawFractal() {
-  const SIDES = 3;
+  const hue = Math.random() * 360;
+  const lineWidth = Math.random() * 11 + 5;
+  const SIDES = 7;
   const maxLevel = 4;
   const diverge = Math.random() * 1 - 0.5;
-  const scale = Math.random() * 0.4 + 0.4;
-  const branches = 6;
+  const scale = Math.random() * 0.2 + 0.4;
+  const branches = 4;
 
   ctx.clearRect(0, 0, SIZE, SIZE);
+  ctx.lineWidth = lineWidth;
   ctx.save();
   ctx.translate(SIZE / 2, SIZE / 2);
   for (let i = 0; i < SIDES; i++) {
-    drawBranch(0, maxLevel, diverge, scale, branches);
+    drawBranch(0, maxLevel, diverge, scale, branches, hue);
     ctx.rotate((Math.PI * 2) / SIDES);
   }
   ctx.restore();
 }
 
-function drawBranch(level, maxLevel, diverge, scale, branches) {
+function drawBranch(level, maxLevel, diverge, scale, branches, hue) {
   if (level > maxLevel) return;
   const branchSize = SIZE * 0.25;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   ctx.moveTo(0, 0);
   ctx.lineTo(branchSize, 0);
@@ -46,9 +50,9 @@ function drawBranch(level, maxLevel, diverge, scale, branches) {
     const ref = branchSize - (branchSize / branches) * i;
     ctx.save();
     ctx.translate(ref, 0);
-    ctx.scale(scale, scale);
+    ctx.scale(scale, scale, hue);
     ctx.rotate(diverge);
-    drawBranch(level + 1, maxLevel, diverge, scale, branches);
+    drawBranch(level + 1, maxLevel, diverge, scale, branches, hue);
     ctx.restore();
   }
 }
